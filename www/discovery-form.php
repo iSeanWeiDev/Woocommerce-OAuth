b2029095ef7c91f2b1b44b1c76e3ee0d485dd6e0
super-sean1995 <super.sean1995@outlook.com>
2019-05-27 16:06:09 -0400

    OAuth Client version 1.0.0

---
+++ www/discovery-form.php
@@ -0,0 +1,12 @@
+<form action="" method="GET">
+	<input type="hidden" name="step" value="discover" />
+
+	<h2>Step 1: Find the API</h2>
+	<p>
+		<label>
+			Address to start discovery:
+			<input type="url" name="uri" class="uri-input" required />
+		</label>
+		<button type="submit">Begin Discovery</button>
+	</p>
+</form>
